c6d66e85a262dc6b9d59af023bf4a8986923890d
sumi-re01 <owob.viola@gmail.com>
2021-04-13 08:13:07 +0000

    [Add] Itinerary-Plan association

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,6 +1,7 @@
 class Itinerary < ApplicationRecord
 
   belongs_to :user
+  has_many :plans, -> { order(position: :asc)}
 
   validates :date, presence: true
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,8 @@
 class Plan < ApplicationRecord
+
+  belongs_to :itinerary
+  acts_as_list scope: :itinerary
+
+  validates :plan, length:{maximum: 20}
+
 end
