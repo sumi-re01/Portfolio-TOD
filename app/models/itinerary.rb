0f7824065701cfceef6e7a9392d229b3ed3a2e8e
sumi-re01 <owob.viola@gmail.com>
2021-04-13 06:24:55 +0000

    [Add] User-Itinerary association

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,2 +1,6 @@
 class Itinerary < ApplicationRecord
+
+  belongs_to :user
+
+  validates :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   attachment :profile_image
 
   has_many :sns_credentials, dependent: :destroy
+  has_many :itineraries, dependent: :destroy
 
 
   def self.from_omniauth(auth)
