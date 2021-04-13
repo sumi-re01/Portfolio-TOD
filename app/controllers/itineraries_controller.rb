03b98d1121f7bbd6971a1aaf8c4ea0e19e8363b0
sumi-re01 <owob.viola@gmail.com>
2021-04-13 13:18:40 +0000

    [Fix] users/show.html indent with 4space [Add] cntroller comment about create render

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,13 +1,16 @@
 class ItinerariesController < ApplicationController
 
+    # user/show内
     def create
       # current_userについては見直し必。繰り返しをまとめる
       itinerary = Itinerary.new(itinerary_params)
       itinerary.user_id = current_user.id
       if itinerary.save
+        # itineraries/index.js.erbへ飛んでusers/showのitineraries一覧を取得
         @itineraries = current_user.itineraries.all
         render :index
       else
+        # users/show.html.erbに戻る
         @itineraries = current_user.itineraries.all
         render 'users/show'
       end
