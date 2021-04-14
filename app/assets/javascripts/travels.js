3934494876d5384617c22618b4e4570952fbc484
sumi-re01 <owob.viola@gmail.com>
2021-04-14 06:20:30 +0000

    [Add] Sortable list

---
+++ app/assets/javascripts/travels.js
@@ -0,0 +1,21 @@
+/* global Sortable*/
+/* global $ */
+
+$(function() {
+  const el = document.getElementById("sortable_list");
+  if (el != null) {
+    console.log("1");
+    const sortable = Sortable.create(el, {
+       handle: "#bar",
+       delay: 150,
+
+       onUpdate: function (evt) {
+        $.ajax({
+          url: 'travel/' + $("#travel_id").val() + '/sort',
+          type: 'PATCH',
+          data: { from: evt.oldIndex, to: evt.newIndex }
+        });
+      }
+     });
+  }
+});
--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,42 +1,53 @@
 class TravelsController < ApplicationController
+  before_action :set_travel, only: [:destroy, :show, :sort]
 
-  # user/show内
-    def create
-      # current_userについては見直し必。繰り返しをまとめる
-      travel = Travel.new(travel_params)
-      travel.user_id = current_user.id
-      if travel.save
-        # travels/index.js.erbへ飛んでusers/showのtravel一覧を取得
-        @travels = current_user.travels.all
-        render :index
-      else
-        # users/show.html.erbに戻る
-        @travels = current_user.travels.all
-        render 'users/show'
-      end
-    end
+# user/show内
 
-    def destroy
-      travel = Travel.find(params[:id])
-      travel.destroy
-
+  def create
+    # current_userについては見直し必。繰り返しをまとめる
+    travel = Travel.new(travel_params)
+    travel.user_id = current_user.id
+    if travel.save
+      # travels/index.js.erbへ飛んでusers/showのtravel一覧を取得
       @travels = current_user.travels.all
-      render 'travels/index'
+      render :index
+    else
+      # users/show.html.erbに戻る
+      @travels = current_user.travels.all
+      render 'users/show'
     end
+  end
+
+  def destroy
+    @travel.destroy
+
+    @travels = current_user.travels.all
+    # index.js.erbへ
+    render 'travels/index'
+  end
+
+# travels/show内
+
+  def show
+    @travel_plan = TravelPlan.new
+  end
+
+  # travels/_plansのposition更新
+  def sort
+    travel_plan = @travel.travel_plans[params[:from].to_i]
+    travel_plan.insert_at(params[:to].to_i + 1)
+    head :ok
+  end
 
 
-    def show
-      @travel = Travel.find(params[:id])
-      @travel_plan = TravelPlan.new
-    end
+  private
 
-    def update
-    end
+  def set_travel
+    @travel = Travel.find(params[:id])
+  end
 
-    private
-
-    def travel_params
-       params.require(:travel).permit(:title, :date, :user_id)
-    end
+  def travel_params
+    params.require(:travel).permit(:title, :date, :user_id)
+  end
 
 end
