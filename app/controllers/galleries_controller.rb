ff65196f8e5f702fb833c515e6b66b3e54b3e6e2
sumi-re01 <owob.viola@gmail.com>
2021-04-16 04:15:50 +0000

    [Add] Gallery Index (Layout yet)

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,9 +1,11 @@
 class GalleriesController < ApplicationController
+  before_action :set_gallery, only: [:edit, :update, :show, :destroy]
+  before_action :set_own_travels, only: [:new, :update]
+
 
   def new
     # 紐付けたいtravelを取得
     # @travel = Travel.where(id: params[:gallery][:travel_id])
-    @travels = current_user.travels.all
     @gallery = Gallery.new
   end
 
@@ -19,24 +21,37 @@ class GalleriesController < ApplicationController
   end
 
   def edit
-    @gallery = Gallery.find(patams[:id])
   end
 
   def update
   end
 
   def destroy
+    if @gallery.user_id = current_user.id
+      @gallery.destroy
+    else
+      redirect_to gallery_path(@gallery)
+    end
   end
 
   def show
-    @gallery = Gallery.find(params[:id])
     @user = @gallery.travel.user
   end
 
   def index
+    @galleries = Gallery.all
   end
 
+
   private
+
+  def set_own_travels
+    @travels = current_user.travels.all
+  end
+
+  def set_gallery
+    @gallery = Gallery.find(params[:id])
+  end
 
   def gallery_params
     params.require(:gallery).permit(:travel_id, :text, :address, :latitude, :longitude, :public_status, { images: [] })
