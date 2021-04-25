20f6c6df20a9e9e5ad0cba1734e81d0de7cb0642
sumi-re01 <owob.viola@gmail.com>
2021-04-25 11:58:49 +0000

    [Add] gallery_spec

--- spec/models/gallery_spec.rb
+++ spec/models/gallery_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Gallery, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with " do
+    user = User.create(
+      name: "たなか",
+      email: "tester@example.com",
+      password: "testpass"
+      )
+    travel = user.travels.create(
+      title: "旅行名",
+      date: "2021/01/01"
+      )
+    gallery = travel.gallery.create(
+        text: "xxxxxxxxxxxxx",
+        address: "Tokyo",
+        images: "xxxxxxx.jpg"
+        )
+      expect(gallery).to be_valid
+  end
 end
