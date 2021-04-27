d5fd815714494c2457a34ea02be104198f20ade1
sumi-re01 <owob.viola@gmail.com>
2021-04-27 06:26:16 +0000

    [Add] Refile.secret_key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0cafd90c9800ab268183d0f4c55529506ce4e176883dc17566967d12ae34efb4cae9b9eba5869f4c73c6e5981ad6278166798e81883a4693caff4f9745ad
+f959'
